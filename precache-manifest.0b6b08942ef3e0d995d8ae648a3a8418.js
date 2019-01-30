cd45aca22d09d8ad2be85e304ff1d51d0c8e7c61
Zack <zack1444@gmail.com>
2019-01-30 09:56:23 -0800

    Updates

---
+++ precache-manifest.0b6b08942ef3e0d995d8ae648a3a8418.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "087a0ff524e22f065869",
+    "url": "/18-ClickyGame/static/js/runtime~main.087a0ff5.js"
+  },
+  {
+    "revision": "93d85ffe8136a5e5f465",
+    "url": "/18-ClickyGame/static/js/main.93d85ffe.chunk.js"
+  },
+  {
+    "revision": "8d0e6daf5a4074e9364a",
+    "url": "/18-ClickyGame/static/js/1.8d0e6daf.chunk.js"
+  },
+  {
+    "revision": "0aa0e001f2e02b098484b02f1bbb8ea6",
+    "url": "/18-ClickyGame/index.html"
+  }
+];
